c8453dd2576e1326a441b025fddfbde776a9e945
Andres Canal <andres@inakanetworks.com>
2016-05-26 16:09:47 -0300

    changed mam api and added way to fetch possible fields to filter when fetching mam

--- Mangosta/Library/XMPPMessageArchiveManagement.m
+++ Mangosta/Library/XMPPMessageArchiveManagement.m
@@ -1,8 +1,7 @@
 //
 //  XMPPMessageArchiveManagement.m
-//  Mangosta
 //
-//  Created by Tom Ryan on 4/8/16.
+//  Created by Andres Canal on 4/8/16.
 //  Copyright © 2016 Inaka. All rights reserved.
 //
 
@@ -10,61 +9,42 @@
 #import "XMPPFramework.h"
 #import "XMPPLogging.h"
 #import "XMPPIDTracker.h"
-#import "XMPPMessage+XEP_0313.h"
-#import "NSXMLElement+XEP_0059.h"
-
-#if ! __has_feature(objc_arc)
-#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
-#endif
-
-// Log levels: off, error, warn, info, verbose
-// Log flags: trace
-#if DEBUG
-static const int xmppLogLevel = XMPP_LOG_LEVEL_VERBOSE; // | XMPP_LOG_FLAG_TRACE;
-#else
-static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
-#endif
 
 #define XMLNS_XMPP_MAM @"urn:xmpp:mam:1"
 
-@interface XMPPMessageArchiveManagement ()
-@end
-
 @implementation XMPPMessageArchiveManagement
 
-- (instancetype)initWithDispatchQueue:(dispatch_queue_t)queue {
-	self = [super initWithDispatchQueue:queue];
-	
-	if (self) {
-
-	}
-	
-	return self;
-}
-
-- (void)retrieveMessageArchiveFrom:(XMPPJID *)userJID withResultSet:(XMPPResultSet *)resultSet {
+- (void)retrieveMessageArchiveWithFields:(NSArray *)fields withResultSet:(XMPPResultSet *)resultSet {
 	dispatch_block_t block = ^{
 		
-		if ([xmppIDTracker numberOfIDs] == 0) {
-			
-			XMPPIQ *iq = [XMPPIQ iqWithType:@"set"];
-			[iq addAttributeWithName:@"id" stringValue:[XMPPStream generateUUID]];
-			
-			DDXMLElement *queryElement = [DDXMLElement elementWithName:@"query" xmlns:XMLNS_XMPP_MAM];
-			[queryElement addAttributeWithName:@"queryId" stringValue:[XMPPStream generateUUID]];
-			[iq addChild:queryElement];
-			
-			DDXMLElement *xElement = [DDXMLElement elementWithName:@"x" xmlns:@"jabber:x:data"];
-			[xElement addAttributeWithName:@"type" stringValue:@"submit"];
-			[xElement addChild:[self fieldWithVar:@"FORM_TYPE" type:@"hidden" andValue:@"urn:xmpp:mam:1"]];
-			[xElement addChild:[self fieldWithVar:@"with" type:nil andValue:userJID.full]];
-
-			[queryElement addChild:xElement];
-			[queryElement addChild:resultSet];
-			
-			[xmppIDTracker addElement:iq target:self selector:@selector(enableMessageArchiveIQ:withInfo:) timeout:XMPPIDTrackerTimeoutNone];
-			[xmppStream sendElement:iq];
+		XMPPIQ *iq = [XMPPIQ iqWithType:@"set"];
+		[iq addAttributeWithName:@"id" stringValue:[XMPPStream generateUUID]];
+		
+		DDXMLElement *queryElement = [DDXMLElement elementWithName:@"query" xmlns:XMLNS_XMPP_MAM];
+		[queryElement addAttributeWithName:@"queryid" stringValue:[XMPPStream generateUUID]];
+		[iq addChild:queryElement];
+		
+		DDXMLElement *xElement = [DDXMLElement elementWithName:@"x" xmlns:@"jabber:x:data"];
+		[xElement addAttributeWithName:@"type" stringValue:@"submit"];
+		[xElement addChild:[XMPPMessageArchiveManagement fieldWithVar:@"FORM_TYPE" type:@"hidden" andValue:@"urn:xmpp:mam:1"]];
+		
+		for (DDXMLElement *field in fields) {
+			[xElement addChild:field];
 		}
+		
+		[queryElement addChild:xElement];
+		
+		if (resultSet) {
+			[queryElement addChild:resultSet];
+		}
+		
+		[xmppIDTracker addElement:iq
+						   target:self
+						 selector:@selector(handleMessageArchiveIQ:withInfo:)
+						  timeout:60];
+		
+		[xmppStream sendElement:iq];
+		
 	};
 	
 	if (dispatch_get_specific(moduleQueueTag)) {
@@ -74,7 +54,21 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 	}
 }
 
-- (DDXMLElement *) fieldWithVar:(NSString *) var type:(NSString *) type andValue:(NSString *) value {
+- (void)handleMessageArchiveIQ:(XMPPIQ *)iq withInfo:(XMPPBasicTrackingInfo *)trackerInfo {
+	
+	if ([[iq type] isEqualToString:@"result"]) {
+		
+		DDXMLElement *finElement = [iq elementForName:@"fin" xmlns:XMLNS_XMPP_MAM];
+		DDXMLElement *setElement = [finElement elementForName:@"set" xmlns:@"http://jabber.org/protocol/rsm"];
+		
+		XMPPResultSet *resultSet = [XMPPResultSet resultSetFromElement:setElement];
+		[multicastDelegate xmppMessageArchiveManagement:self didFinishReceivingMessagesWithSet:resultSet];
+	} else {
+		[multicastDelegate xmppMessageArchiveManagement:self didFailToReceiveMessages:iq];
+	}
+}
+
++ (DDXMLElement *)fieldWithVar:(NSString *)var type:(NSString *)type andValue:(NSString *)value {
 	DDXMLElement *field = [DDXMLElement elementWithName:@"field"];
 	[field addAttributeWithName:@"var" stringValue:var];
 	
@@ -90,11 +84,43 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 	return field;
 }
 
+- (void)retrieveFormFields {
+	
+	dispatch_block_t block = ^{
+		
+		XMPPIQ *iq = [XMPPIQ iqWithType:@"get"];
+		[iq addAttributeWithName:@"id" stringValue:[XMPPStream generateUUID]];
+		
+		DDXMLElement *queryElement = [DDXMLElement elementWithName:@"query" xmlns:XMLNS_XMPP_MAM];
+		[iq addChild:queryElement];
+		
+		[xmppIDTracker addElement:iq
+						   target:self
+						 selector:@selector(handleFormFieldsIQ:withInfo:)
+						  timeout:60];
+		
+		[xmppStream sendElement:iq];
+	};
+	
+	if (dispatch_get_specific(moduleQueueTag)) {
+		block();
+	} else {
+		dispatch_sync(moduleQueue, block);
+	}
+}
+
+- (void)handleFormFieldsIQ:(XMPPIQ *)iq withInfo:(XMPPBasicTrackingInfo *)trackerInfo {
+	
+	if ([[iq type] isEqualToString:@"result"]) {
+		[multicastDelegate xmppMessageArchiveManagement:self didReceiveFormFields:iq];
+	} else {
+		[multicastDelegate xmppMessageArchiveManagement:self didFailToReceiveFormFields:iq];
+	}
+}
+
 - (BOOL)activate:(XMPPStream *)aXmppStream {
-	XMPPLogTrace();
 	
 	if ([super activate:aXmppStream]) {
-		XMPPLogVerbose(@"%@: Activated", THIS_FILE);
 		xmppIDTracker = [[XMPPIDTracker alloc] initWithDispatchQueue:moduleQueue];
 		
 		
@@ -104,7 +130,6 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 }
 
 - (void)deactivate {
-	XMPPLogTrace();
 	
 	dispatch_block_t block = ^{ @autoreleasepool {
 		[xmppIDTracker removeAllIDs];
@@ -120,9 +145,8 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 	[super deactivate];
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark XMPPStream Delegate
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 - (BOOL)xmppStream:(XMPPStream *)sender didReceiveIQ:(XMPPIQ *)iq
 {
 	NSString *type = [iq type];
@@ -134,25 +158,13 @@ static const int xmppLogLevel = XMPP_LOG_LEVEL_WARN;
 	return NO;
 }
 
-- (void)enableMessageArchiveIQ:(XMPPIQ *)iq withInfo:(XMPPBasicTrackingInfo *)trackerInfo {
-
-	if ([[iq type] isEqualToString:@"result"]) {
-
-		DDXMLElement *finElement = [iq elementForName:@"fin" xmlns:XMLNS_XMPP_MAM];
-		DDXMLElement *setElement = [finElement elementForName:@"set" xmlns:@"http://jabber.org/protocol/rsm"];
-
-		XMPPResultSet *resultSet = [XMPPResultSet resultSetFromElement:setElement];
-		[multicastDelegate xmppMessageArchiveManagement:self didFinishReceivingMessagesWithSet:resultSet];
-	} else {
-		[multicastDelegate xmppMessageArchiveManagement:self didReceiveError:iq];
-	}
-}
-
-- (XMPPMessage *)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message {
-	if ([message isMessageArchive]) {
+- (void)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message {
+	DDXMLElement *result = [message elementForName:@"result" xmlns:XMLNS_XMPP_MAM];
+	DDXMLElement *forwarded = [result elementForName:@"forwarded"];
+	
+	if (forwarded) {
 		[multicastDelegate xmppMessageArchiveManagement:self didReceiveMAMMessage:message];
 	}
-	return message;
 }
 
 @end
