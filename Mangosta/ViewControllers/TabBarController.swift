204f2074ccee62d1d2698b4d68e62c0a116195d4
Sergio E. Abraham <sergio.abraham@inakanetworks.com>
2016-12-28 15:55:03 -0300

    Revert "[sergio[sergio.iconsFix] WIP forcing tabBarItem.image for testing purposes."
    This reverts commit 1a13a7369275e37f99efdc254ad8f3bc839391e6.

--- Mangosta/ViewControllers/TabBarController.swift
+++ Mangosta/ViewControllers/TabBarController.swift
@@ -10,26 +10,11 @@ import UIKit
 
 class TabBarController: UITabBarController {
 
-	@IBOutlet weak var thisTabBar: UITabBar!
-	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGrayColor()], forState: UIControlState.Normal)
 		UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.whiteColor()], forState: UIControlState.Selected)
-
-	}
-	override func viewWillAppear(animated: Bool) {
-		
-		self.thisTabBar.items![0].image = UIImage(named: "Social Filled")
-		
-		self.thisTabBar.items![0].selectedImage = UIImage(named: "Social")
-		
-		self.thisTabBar.items![1].image = UIImage(named: "Social Filled")
-		
-		self.thisTabBar.items![1].selectedImage = UIImage(named: "Social")
-		
-		
 		
 	}
 }
