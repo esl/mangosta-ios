6eb71726524d1016244c66a59e3f055ab9233e09
Sergio E. Abraham <sergio.abraham@inakanetworks.com>
2016-12-30 15:48:39 -0300

    [sergio.TabBarIconsSuperFix] Guards the tab bar view controller to set ups the image when the sub storyboard and view controller are instantiated.

--- Mangosta/ViewControllers/TabBarController.swift
+++ Mangosta/ViewControllers/TabBarController.swift
@@ -16,5 +16,15 @@ class TabBarController: UITabBarController {
 		UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.lightGrayColor()], forState: UIControlState.Normal)
 		UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.whiteColor()], forState: UIControlState.Selected)
 		
+		guard let socialMediaViewController = self.viewControllers?[1] as? SocialMediaViewController else {
+			return
+		}
+		
+		// Fixes a nasty problem is the sub storyboard controller's viewDidLoad is not still intantiated. 
+		
+		socialMediaViewController.tabBarItem.title = NSLocalizedString("Social", comment: "")
+		socialMediaViewController.tabBarItem.image = UIImage(named: "Social")!
+		socialMediaViewController.tabBarItem.selectedImage = UIImage(named: "Social Filled")
+		
 	}
 }
