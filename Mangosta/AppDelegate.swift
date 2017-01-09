b84df16652d593c2a34dee6f1ab2be93c6acb0c1
Sergio E. Abraham <sergio.abraham@inakanetworks.com>
2017-01-09 14:09:20 -0300

    [sergio.ActiveInactive] Moved code XEP-0352 Active/Inactive to be automatic controlled by app lifecycle.

--- Mangosta/AppDelegate.swift
+++ Mangosta/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}
 
 	func applicationWillResignActive(application: UIApplication) {
-		
+		self.xmppController?.xmppStream.sendElement(XMPPElement.indicateInactiveElement())
 	}
 
 	func applicationDidEnterBackground(application: UIApplication) {
@@ -39,11 +39,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}
 
 	func applicationDidBecomeActive(application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+		self.xmppController?.xmppStream.sendElement(XMPPElement.indicateActiveElement())
 	}
 
 	func applicationWillTerminate(application: UIApplication) {
-		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+		self.xmppController?.xmppStream.sendElement(XMPPElement.indicateInactiveElement())
 	}
 
 
