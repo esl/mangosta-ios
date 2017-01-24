d7620fc82ee167396c443aa5f05cc16cb915fcd3
Sergio E. Abraham <sergio.abraham@inakanetworks.com>
2017-01-24 16:55:54 -0300

    [sergio.Swift2.3approach] Cleaning repo from Pods.

--- Pods/Target Support Files/Pods-Mangosta/Pods-Mangosta-frameworks.sh
+++ Pods/Target Support Files/Pods-Mangosta/Pods-Mangosta-frameworks.sh
@@ -82,18 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/CocoaAsyncSocket/CocoaAsyncSocket.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/KissXML/KissXML.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/XMPPFramework/XMPPFramework.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/CocoaAsyncSocket/CocoaAsyncSocket.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/CocoaLumberjack/CocoaLumberjack.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/KissXML/KissXML.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/XMPPFramework/XMPPFramework.framework"
-fi
