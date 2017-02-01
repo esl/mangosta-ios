f56188e08096a4b421d24cd522a7f8dbfe99a701
Sergio E. Abraham <sergio.abraham@inakanetworks.com>
2017-02-01 18:27:44 -0300

    [sergio.PubSub] PubSub setup inside xmppController. Adds microblog capability inside presence and creates my node.

---
+++ Mangosta/Model/PubSub+Microblogging.swift
@@ -0,0 +1,21 @@
+//
+//  PubSub+Microblogging.swift
+//  Mangosta
+//
+//  Created by Sergio Abraham on 1/31/17.
+//  Copyright © 2017 Inaka. All rights reserved.
+//
+
+import Foundation
+
+class MicroBlogging {
+    func sendMicroblogCapabilities() {
+        
+    }
+    func retrieveFromNode(node: String) {
+        
+    }
+    func publishBlogEntry(tittle: String, content: String) {
+        
+    }
+}
