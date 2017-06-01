028b9597c217c99cc93236d35e666bdd8a01e212
Piotr Wegrzynek <pwe@trifork.com>
2017-06-01 10:47:28 +0200

    Fix a MUC Light-MAM interactions regression
    XMPPRoomLightCoreDataStorage was not conforming to a protocol due to a missing marker category implementation

---
+++ Mangosta/Library/XMPPRoomLightCoreDataStorage+XMPPMessageArchiveManagementAwareRoomStorage.m
@@ -0,0 +1,13 @@
+//
+//  XMPPRoomLightCoreDataStorage+XMPPMessageArchiveManagementAwareRoomStorage.m
+//  Mangosta
+//
+//  Created by Piotr Wegrzynek on 30/05/2017.
+//  Copyright © 2017 Inaka. All rights reserved.
+//
+
+#import "XMPPRoomLightCoreDataStorage+XMPPMessageArchiveManagementAwareRoomStorage.h"
+
+@implementation XMPPRoomLightCoreDataStorage (XMPPMessageArchiveManagementAwareRoomStorage)
+
+@end
