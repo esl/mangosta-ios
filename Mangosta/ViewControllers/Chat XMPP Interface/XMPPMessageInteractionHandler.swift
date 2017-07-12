74d38f89cc92692a469c1daa7e40e8f935bebaf4
Piotr Wegrzynek <pwe@trifork.com>
2017-07-12 16:12:19 +0200

    QuickLook-based file previews

--- Mangosta/ViewControllers/Chat XMPP Interface/XMPPMessageInteractionHandler.swift
+++ Mangosta/ViewControllers/Chat XMPP Interface/XMPPMessageInteractionHandler.swift
@@ -7,6 +7,8 @@
 //
 
 import ChattoAdditions
+import QuickLook
+import XMPPFramework
 
 class XMPPTextMessageInteractionHandler<ViewModel: TextMessageViewModelProtocol>: BaseMessageInteractionHandlerProtocol {
     
@@ -18,9 +20,10 @@ class XMPPTextMessageInteractionHandler<ViewModel: TextMessageViewModelProtocol>
     func userDidEndLongPressOnBubble(viewModel: ViewModel) {}
 }
 
-class XMPPPhotoMessageInteractionHandler: BaseMessageInteractionHandlerProtocol {
+class XMPPPhotoMessageInteractionHandler: NSObject, BaseMessageInteractionHandlerProtocol, QLPreviewControllerDelegate {
     
     private unowned let contextViewController: UIViewController
+    private var currentPreviewControllerDataSource: QLPreviewControllerDataSource?
     
     init(contextViewController: UIViewController) {
         self.contextViewController = contextViewController
@@ -32,6 +35,47 @@ class XMPPPhotoMessageInteractionHandler: BaseMessageInteractionHandlerProtocol 
             return
         }
         
+        retryTransfer(for: viewModel, with: retryInitiator)
+    }
+    
+    func userDidTapOnBubble(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {
+        switch viewModel._photoMessage.transferMonitor.state {
+        case let .pending(isTransferFailed, transferInitiator):
+            if isTransferFailed {
+                // notify the user that a transfer has failed before and ask for confirmation before retrying
+                retryTransfer(for: viewModel, with: transferInitiator)
+            } else {
+                // no previous failure information available; assume this is the initial transfer attempt and proceed without confirmation
+                transferInitiator.invoke()
+            }
+            
+        case .done(let previewFileUrl):
+            if let previewControllerDataSource = viewModel._photoMessage._messageModel.previewControllerDataSource(withFileUrl: previewFileUrl) {
+                let previewController = QLPreviewController()
+
+                previewController.dataSource = previewControllerDataSource
+                currentPreviewControllerDataSource = previewControllerDataSource
+
+                previewController.delegate = self
+
+                contextViewController.present(previewController, animated: true, completion: nil)
+            }
+            
+        case .working:
+            break
+        }
+    }
+    
+    // TODO
+    func userDidTapOnAvatar(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {}
+    func userDidBeginLongPressOnBubble(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {}
+    func userDidEndLongPressOnBubble(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {}
+    
+    func previewControllerDidDismiss(_ controller: QLPreviewController) {
+        currentPreviewControllerDataSource = nil
+    }
+    
+    private func retryTransfer(for viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>, with retryInitiator: PhotoMessageTransferInitiator) {
         let message: String
         switch viewModel.transferDirection.value {
         case .upload:
@@ -48,15 +92,35 @@ class XMPPPhotoMessageInteractionHandler: BaseMessageInteractionHandlerProtocol 
         
         contextViewController.present(retryConfirmationSheet, animated: true, completion: nil)
     }
+}
+
+private extension MessageModel {
     
-    func userDidTapOnBubble(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {
-        guard viewModel.transferStatus.value == .idle else {
-            return
+    class PreviewItem: NSObject, QLPreviewControllerDataSource, QLPreviewItem {
+        
+        let title: String
+        let fileUrl: URL
+        
+        var previewItemURL: URL? { return fileUrl }
+        var previewItemTitle: String? { return title }
+        
+        init(title: String, fileUrl: URL) {
+            self.title = title
+            self.fileUrl = fileUrl
+        }
+        
+        func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+            return 1
+        }
+        
+        func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+            return self
         }
     }
     
-    // TODO
-    func userDidTapOnAvatar(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {}
-    func userDidBeginLongPressOnBubble(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {}
-    func userDidEndLongPressOnBubble(viewModel: PhotoMessageViewModel<TransferAwarePhotoMessageModel<MessageModel>>) {}
+    func previewControllerDataSource(withFileUrl previewFileUrl: URL) -> QLPreviewControllerDataSource? {
+        let title = XMPPJID(string: senderId)?.user ?? senderId
+        let previewItem = PreviewItem(title: title, fileUrl: previewFileUrl)
+        return QLPreviewController.canPreview(previewItem) ? previewItem : nil
+    }
 }
